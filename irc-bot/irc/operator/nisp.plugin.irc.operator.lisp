911ba189d0c094db506d0b8e044343c24a059e35
Alex <zc00gii@gmail.com>
2010-06-24 03:56:06 -0400

    Sorta-kinda-not-really-not-at-all finished classes

--- irc-bot/irc/operator/nisp.plugin.irc.operator.lisp
+++ irc-bot/irc/operator/nisp.plugin.irc.operator.lisp
@@ -3,8 +3,6 @@
 
 (in-package :nisp.plugin.operator)
 
-;; (defclass operator)
-
 ;; Hashing functions
 
 (defun hash-password (password)
@@ -17,3 +15,46 @@
 
 (defun verify-password (password hash)
   (hash= (hash-password password) hash))
+
+;; Channel modes class
+
+(defclass channel-modes ()
+  ((name :
+   (modes :accessor chan-modes
+	  :initform ""
+	  :initarg :chan-modes))
+
+;; Privileges class
+
+(defclass privileges ()
+  ((channels  :accessor priv-chans
+	      :initform '((make-instance channels))
+	      :initarg :channels)
+
+   (commands  :accessor priv-coms
+	      :initform '()
+	      :initarg  :commands)))
+
+
+;; Operator class
+
+(defclass operator () 
+  ((nick                 :accessor oper-nick
+			 :initform person
+			 :initarg  :nick)
+
+   (host                 :accessor oper-host
+			 :initform "nick!ident@host"
+			 :initarg :host)
+
+   (password             :accessor oper-password
+			 :initform (hash-password "password")
+			 :initarg  :password)
+
+   (privileges           :accessor oper-privs
+			 :initform (make-instance privileges)
+			 :initarg  :privs)
+
+   (need-password        :accessor oper-need-pass
+			 :initform t
+			 :initarg  :need-pass)))
